4ccfd02c5790231d35804a284490b5660b4d737e
quanle28197 <86665723+quanle28197@users.noreply.github.com>
2022-02-08 14:07:19 +0700

    update

---
+++ src/prime.java
@@ -0,0 +1,31 @@
+public class prime {
+    public static void main(String[] args) {
+
+        int count = 0;
+        int number = 0;
+        System.out.print("There are 20 first prime numbers; ");
+
+        while(count < 20) {
+            if (isPrime(number)) {
+                System.out.printf("%d ,", number);
+                count++;
+            }
+            number++;
+        }
+    }
+
+    private static boolean isPrime(int number) {
+
+        if(number == 2) {
+            return true;
+        }
+        if (number> 2) {
+            for (int i = 2; i < Math.sqrt(number); i++) {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        return false;
+    }
+}
